bc9897fc91b545ae905693d5b2389071273a1f35
Valentin Marmonier <valentin.marmonier@gmail.com>
2014-05-07 08:21:09 +0800

    request and endpoint setup

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,39 @@ console.log(log);
 
 var geo = new Geolocation();
 geo.localize(function (position) {
-	log.send('Position found', 'green');
-	log.send('Lattitude: ' + position.coords.latitude, 'green');
-	log.send('Longitude: ' + position.coords.longitude, 'green');
-	log.send('Accuracy: ' + position.coords.accuracy, 'green');
 
-	log.send('Sending request');
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var accuracy = position.coords.accuracy;
+
+	log.send('Position found', 'green');
+	log.send('Lattitude: ' + latitude, 'green');
+	log.send('Longitude: ' + longitude, 'green');
+	log.send('Accuracy: ' + accuracy, 'green');
+
+	log.send('Preparing the request');
+
+	var url = 'http://localhost:8888/geolocation/build/php/api.php?lat=' + latitude + '&long=' + longitude;
+
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+
+	request.onload = function () {
+		if(request.status >= 200 && request.status < 400) {
+			log.send('Request success', 'green');
+		} else {
+			log.send('Request response error', 'red');
+		}
+	};
+
+	request.onerror = function () {
+		log.send('Request connection error', 'red');
+	};
+
+	request.send();
+
+	log.send('Request sent');
+	log.send(url, 'blue');
 });
 
 log.send('Looking for your position');
