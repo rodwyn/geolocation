502186878141f67154e802cd8f268775e172373e
Valentin Marmonier <valentin.marmonier@gmail.com>
2014-05-07 12:17:30 +0800

    comment and clean

--- src/php/api.php
+++ src/php/api.php
@@ -1,17 +1,14 @@
 <?php
-function getDistance($lat1, $lon1, $lat2, $lon2) {
-	$R = 6371;
-	$dLat = deg2rad($lat2 - $lat1);
-	$dLon = deg2rad($lon2 - $lon1);
-	$a = sin($dLat/2) * sin($dLat/2) +
-		cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
-		sin($dLon/2) * sin($dLon/2);
-
-	$c = 2 * atan2(sqrt(a), sqrt(1-a));
-	$d = R * c;
-	return $d;
-}
-
+/**
+ * Get the distance between two sets of coordinates
+ * using the haversine formula
+ * @param {Float} $lat1
+ * @param {Float} $lon1
+ * @param {Float} $lat2
+ * @param {Float} $lon2
+ * @param {Char} $unit
+ * @return {Float} distance
+ */
 function distance($lat1, $lon1, $lat2, $lon2, $unit) {
 	$theta = $lon1 - $lon2;
 	$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
@@ -29,65 +26,78 @@ function distance($lat1, $lon1, $lat2, $lon2, $unit) {
 	}
 }
 
-$hospitals = array(
-	array(
-		'name' => 'Hello',
-		'latitude' => 43.540585,
-		'longitude' => 58.183594
-	),
-	array(
-		'name' => 'World',
-		'latitude' => 27.281485,
-		'longitude' => 106.040039
-	),
-	array(
-		'name' => 'Foo',
-		'latitude' => 27.319124,
-		'longitude' => 107.508774
-	),
-	array(
-		'name' => 'Bar',
-		'latitude' => 27.948123,
-		'longitude' => 108.286057
-	),array(
-		'name' => 'Shanghai',
-		'latitude' => 32.281328,
-		'longitude' => 119.948730
-	)
-);
-
+// response type is json
 header("content-type:application/json");
 
 if(isset($_GET['lat']) && isset($_GET['long'])) {
-	header("HTTP/1.0 200 Found");
 
-	// get the distance
-	foreach($hospitals as $key => $hospital) {
-		
-		//$distance = distance(floatval($_GET['lat']), floatval($_GET['long']), $hospital['latitude'], $hospital['longitude'], 'K');
-		$distance = distance(floatval($_GET['lat']), floatval($_GET['long']), $hospital['latitude'], $hospital['longitude']);
-		
-		// modify the original array
-		$hospitals[$key]['distance'] = $distance;
+	// all the results
+	$items = array(
+		array(
+			'name' => 'Hello',
+			'latitude' => 43.540585,
+			'longitude' => 58.183594
+		),
+		array(
+			'name' => 'World',
+			'latitude' => 27.281485,
+			'longitude' => 106.040039
+		),
+		array(
+			'name' => 'Foo',
+			'latitude' => 27.319124,
+			'longitude' => 107.508774
+		),
+		array(
+			'name' => 'Bar',
+			'latitude' => 27.948123,
+			'longitude' => 108.286057
+		),array(
+			'name' => 'Shanghai',
+			'latitude' => 32.281328,
+			'longitude' => 119.948730
+		)
+	);
 
+	// get the distance with each of the items
+	foreach($items as $key => $item) {
+		$distance = distance(
+			floatval($_GET['lat']),
+			floatval($_GET['long']),
+			$item['latitude'],
+			$item['longitude'],
+			'K'
+		);
+
+		// add the distance to the original array
+		$items[$key]['distance'] = $distance;
 	}
 
-	// sort by distance
+	// // sort by distance
 	function sortByDistance($a, $b) {
 	    return $a['distance'] - $b['distance'];
 	}
-	usort($hospitals, 'sortByDistance');
+	usort($items, 'sortByDistance');
 
-	// crop
+	// crop the array
 	if(isset($_GET['number'])) {
-		$hospitals = array_slice($hospitals, 0, intval($_GET['number']));
+		$items = array_slice($items, 0, intval($_GET['number']));
 	}
 
-	echo json_encode($hospitals);
+	// send a success header
+	header("HTTP/1.0 200 Found");
+
+	// response body
+	echo(json_encode($items));
 
 } else {
+
+	// missing parameters, send an error header
 	header("HTTP/1.0 404 Not Found");
+
+	// response body
 	echo json_encode(array(
 		'message' => 'Parameters missing'
 	));
+
 }
